2bea0e0cce58f8cafcf01a697da81499d7a80d3e
9-max-6 <mutukumaxwel@gmail.com>
2024-08-02 05:02:19 +0300

    Fix: ipdated the name of the function from "concar" to "concat"

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -5,7 +5,7 @@ module thatcontains the concat function
 """
 
 
-def concar(str1: str, str2: str) -> str:
+def concat(str1: str, str2: str) -> str:
     """a type-annotated function concat
     takes a string str1 and a string str2 as arguments
     and returns a concatenated string
