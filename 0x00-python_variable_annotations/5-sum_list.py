e3fe2a367b14e28664eb684330c296194ee37314
9-max-6 <mutukumaxwel@gmail.com>
2024-08-02 05:09:39 +0300

    Fix: sum(list) from list.sum()

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -8,4 +8,4 @@ def sum_list(input_list: list[float]) -> float:
     """a type-annotated function sum_list which takes a list input_list of
     floats as argument and returns their sum as a float.
     """
-    return input_list.sum()
+    return sum(input_list)
