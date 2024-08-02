837ba55df1de0c5130bf3111377e387bc5c2663e
9-max-6 <mutukumaxwel@gmail.com>
2024-08-02 04:56:05 +0300

    Fix: corrected pycodestyle issues

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -4,9 +4,10 @@
 module thatcontains the add function
 """
 
+
 def add(a: float, b: float) -> float:
     """a type-annotated function add that takes a
     float a and a float b as arguments and returns
-    their sum as a float. 
+    their sum as a float.
     """
     return a + b
