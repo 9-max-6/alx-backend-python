b3afbd8cabfe67726c0dadc130e06aa673ec5df0
9-max-6 <mutukumaxwel@gmail.com>
2024-08-02 05:08:36 +0300

    Chore: attempt at typed lists and variable annotations

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+module: function: sum_list()
+"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """a type-annotated function sum_list which takes a list input_list of
+    floats as argument and returns their sum as a float.
+    """
+    return input_list.sum()
