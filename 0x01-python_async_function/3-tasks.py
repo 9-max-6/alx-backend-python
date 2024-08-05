e9308b57eda94f52acba73a4621dbafff0fa12d9
9-max-6 <mutukumaxwel@gmail.com>
2024-08-05 16:22:32 +0300

    Fix: fixed pycodestyle issues

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -6,10 +6,10 @@ import asyncio
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
+
 def task_wait_random(max_delay: int) -> asyncio.Task:
     """A function to take an int max_delay and return
     an asyncio.Task
     """
 
     return asyncio.create_task(wait_random(max_delay))
-
