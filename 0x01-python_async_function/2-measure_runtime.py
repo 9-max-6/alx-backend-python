60dca9c97844dc42df3c0214dc5a46d63625b894
9-max-6 <mutukumaxwel@gmail.com>
2024-08-05 16:00:09 +0300

    Chore: did the 3rd question for measuring the runtime of async functions. Had to create an event loop in order to determine the average runtime of the multiple coroutines.

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -2,10 +2,15 @@
 """module: function measure_time()
 """
 import asyncio
+import time
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-async def measure_time(n: int, max_delay: float):
+def measure_time(n: int, max_delay: int):
     """measures the total execution time for wait_n(n, max_delay),
     and returns total_time / n.
     Your function should return a float."""
+    s = time.perf_counter()
+    asyncio.run(wait_n(n, max_delay))
+    elapsed = time.perf_counter() - s
+    return elapsed / n
