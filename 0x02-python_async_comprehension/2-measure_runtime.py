d23fede07d23e8dfc8bd306f64a54eb0fbe81294
9-max-6 <mutukumaxwel@gmail.com>
2024-08-06 09:52:08 +0300

    Chore: attempt at the final question in the project. Measuring the runtime using asyncio.gather()

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,1 +1,20 @@
 #!/usr/bin/env python3
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+import asyncio
+import time
+
+
+async def measure_runtime() -> float:
+    """a function to measure the runtime of four coroutines
+    all async_comprehension() tasks
+    """
+    now = time.perf_counter()
+    await asyncio.gather(async_comprehension(),
+                   async_comprehension(),
+                   async_comprehension(),
+                   async_comprehension()
+                   )
+    after = time.perf_counter()
+
+    return after - now
