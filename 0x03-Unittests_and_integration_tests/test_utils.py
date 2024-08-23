1d8c94f65eece7c8dd17a0ab06565dbe7d04b3bf
9-max-6 <mutukumaxwel@gmail.com>
2024-08-23 10:31:40 +0300

    Chore: attempted the second question in the series. Asserting the message raised alogn with a keyerror is  a real problem

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -17,3 +17,12 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map(self, nested_map, path, expected):
         """test that the method returns- supposed to return"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
+    
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b"))
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        """A function to assert that a keyerror is raised for some inputs."""
+        with self.assertRaises(KeyError):
+            access_nested_map(nested_map, path)
