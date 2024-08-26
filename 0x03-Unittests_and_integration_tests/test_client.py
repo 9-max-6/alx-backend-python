19c17cb53ddc841bc967e013b82ef06320c9b99e
9-max-6 <mutukumaxwel@gmail.com>
2024-08-26 16:40:15 +0300

    Chore: asserting the execution of the test

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""testsuite"""
+import unittest
+import utils
+from unittest.mock import patch, MagicMock
+from parameterized import parameterized
+from typing import Dict
+from client import GithubOrgClient
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """class: tests"""
+
+    @parameterized.expand([
+        ("google", {'login': "google"}),
+        ("abc", {"login": "abc"}),
+    ])
+    @patch('utils.get_json')
+    def test_org(
+        self,
+        org: str,
+        resp: Dict,
+        mock_get_json: MagicMock
+        ) -> None:
+        """a function to test the GithubOrgClient.org"""
+        mock = MagicMock(return_value=resp)
+        mock_get_json.return_value = mock
+
+        client = GithubOrgClient(org)
+        self.assertEqual(client.org(), resp)
+        expected_arg = f"https://api.github.com/orgs/{org}"
+        mock_get_json.assert_called_once_with(expected_arg)
