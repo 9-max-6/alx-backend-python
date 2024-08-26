f82915852ebd470eee212d57cb691b92e61b8130
9-max-6 <mutukumaxwel@gmail.com>
2024-08-26 19:28:05 +0300

    Chore: asserting the return value of client.public_repos()

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 """testsuite"""
 import unittest
-import client
 from unittest.mock import patch, MagicMock, PropertyMock
 from parameterized import parameterized
 from typing import Dict
 from client import GithubOrgClient
+from fixtures import TEST_PAYLOAD
 
 
 class TestGithubOrgClient(unittest.TestCase):
@@ -38,3 +38,26 @@ class TestGithubOrgClient(unittest.TestCase):
                 GithubOrgClient("google")._public_repos_url,
                 TEST_PAYLOAD.get("repos_url")
                 )
+
+    @patch('client.get_json')
+    def test_public_repos(self, mock_get_json) -> None:
+        """A function to test the publick repos function of the GithubOrgClient
+        steps
+        1. Mock get json. make it return a payload
+        2. Mock _public_repos_url to return something
+        3. Assert that both return the same thing.
+        4. public_repos ->
+        """
+        mock_get_json.return_value = TEST_PAYLOAD[0][1]
+        expected = TEST_PAYLOAD[0][2]
+        prop_string = 'client.GithubOrgClient._public_repos_url'
+
+        with patch(prop_string, new_callable=PropertyMock) as mock:
+            mock.return_value = TEST_PAYLOAD[0][0]["repos_url"]
+            client = GithubOrgClient("google")
+            resp = client.public_repos()
+
+            self.assertEqual(resp, expected)
+            mock.assert_called_once()
+
+        mock_get_json.assert_called_once()
